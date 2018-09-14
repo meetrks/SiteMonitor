0531dd94048df2866812060f135316929159eeff
Rahul <skrsinghrahul@gmail.com>
2018-09-14 23:58:50 +0530

    Add(static): setup static and favicon

--- sitemonitor/urls.py
+++ sitemonitor/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/st/logo/favicon.png', permanent=True)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^favicon\.ico$', favicon_view),
 ]
