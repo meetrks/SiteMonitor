3d9e03e108fc90b3e57085174a3e8a71948e0252
Rahul <skrsinghrahul@gmail.com>
2018-10-08 23:41:10 +0530

    setting: use sms settings from model

--- base/send_sms.py
+++ base/send_sms.py
@@ -1,17 +1,18 @@
-from django.conf import settings
-
 from twilio.rest import Client
 
-client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+from settings.models import TwilioSetting
+
+twilio_obj = TwilioSetting.objects.first()
+client = Client(twilio_obj.account_sid, twilio_obj.auth_token) if twilio_obj else None
 
 
 def send_sms(users, site):
     site_name = site.site_name
     site_url = site.site_url
     for user in users:
-        if user.mobile:
+        if user.mobile and client:
             client.messages.create(
                 to=user.mobile,
-                from_=settings.TWILIO_PHONE_NUMBER,
-                body="You site is down. Site Name- " + site_name + ", Site URL- " + site_url
+                from_=twilio_obj.phone_number,
+                body="You site is not responding. Site Name- " + site_name + ", Site URL- " + site_url
             )
